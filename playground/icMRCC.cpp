4bb06ae9b7be508faf3d717e38d1aaacb5ee7f07
Robert Adam <dev@robert-adam.de>
2023-06-26 16:11:15 +0200

    Added first MRCC example (WIP)

---
+++ playground/icMRCC.cpp
@@ -0,0 +1,118 @@
+#include <SeQuant/core/expr.hpp>
+#include <SeQuant/core/index.hpp>
+#include <SeQuant/core/op.hpp>
+#include <SeQuant/core/sequant.hpp>
+#include <SeQuant/core/space.hpp>
+#include <SeQuant/core/tensor.hpp>
+#include <SeQuant/core/wick.hpp>
+
+#include <iostream>
+
+using namespace sequant;
+
+static IndexFactory idxFactory(nullptr, 1);
+
+// Define aliases for index spaces
+static auto active = IndexSpace::active_unoccupied;
+static auto occ = IndexSpace::occupied;
+static auto virt = IndexSpace::inactive_unoccupied;
+static auto general = IndexSpace::complete;
+
+void setMRCCConvention() {
+  IndexSpace::register_instance(L"f", IndexSpace::frozen_occupied,
+                                IndexSpace::nullqns, true);
+  IndexSpace::register_instance(L"z", IndexSpace::inactive_occupied,
+                                IndexSpace::nullqns, true);
+  IndexSpace::register_instance(L"y", IndexSpace::active_occupied,
+                                IndexSpace::nullqns, true);
+  IndexSpace::register_instance(L"o", IndexSpace::occupied, IndexSpace::nullqns,
+                                true);
+  IndexSpace::register_instance(L"a", IndexSpace::active_unoccupied,
+                                IndexSpace::nullqns, true);
+  IndexSpace::register_instance(L"v", IndexSpace::inactive_unoccupied,
+                                IndexSpace::nullqns, true);
+  IndexSpace::register_instance(L"x", IndexSpace::unoccupied,
+                                IndexSpace::nullqns, true);
+  IndexSpace::register_instance(L"w", IndexSpace::all_active,
+                                IndexSpace::nullqns, true);
+  IndexSpace::register_instance(L"v", IndexSpace::all, IndexSpace::nullqns,
+                                true);
+  IndexSpace::register_instance(L"u", IndexSpace::other_unoccupied,
+                                IndexSpace::nullqns, true);
+  IndexSpace::register_instance(L"t", IndexSpace::complete_unoccupied,
+                                IndexSpace::nullqns, true);
+  IndexSpace::register_instance(L"p", IndexSpace::complete, IndexSpace::nullqns,
+                                true);
+
+  TensorCanonicalizer::set_cardinal_tensor_labels(
+      {L"t", L"f", L"g", L"{C_0}", L"{C_0^\\dagger}"});
+}
+
+ExprPtr make_op(Tensor tensor) {
+  auto bra = tensor.bra();
+  auto ket = tensor.ket();
+  return ex<Tensor>(std::move(tensor)) *
+         ex<FNOperator>(std::move(bra), std::move(ket),
+                        get_default_context().vacuum());
+}
+
+Index create_index(const IndexSpace::Type &type) {
+  return idxFactory.make(IndexSpace::instance(type));
+}
+
+ExprPtr f() {
+  return make_op(Tensor(L"f", std::vector<Index>{create_index(general)},
+                        std::vector<Index>{create_index(general)},
+                        Symmetry::antisymm));
+}
+
+ExprPtr g() {
+  return make_op(Tensor(
+      L"g", std::vector<Index>{create_index(general), create_index(general)},
+      std::vector<Index>{create_index(general), create_index(general)},
+      Symmetry::antisymm));
+}
+
+ExprPtr c0(std::size_t nAct) {
+  std::vector<Index> indices;
+  for (std::size_t i = 0; i < nAct; ++i) {
+    indices.push_back(create_index(active));
+  }
+
+  return make_op(
+      Tensor(L"{C_0}", indices, std::vector<Index>{}, Symmetry::antisymm));
+}
+
+ExprPtr c0dagger(std::size_t nAct) {
+  std::vector<Index> indices;
+  for (std::size_t i = 0; i < nAct; ++i) {
+    indices.push_back(create_index(active));
+  }
+
+  return make_op(
+      Tensor(L"{C_0^\\dagger}", std::vector<Index>{}, indices, Symmetry::antisymm));
+}
+
+ExprPtr t() {
+  return make_op(Tensor(L"t", std::vector<Index>{create_index(active)},
+                        std::vector<Index>{create_index(occ)},
+                        Symmetry::antisymm));
+}
+
+int main() {
+  set_default_context(SeQuant(Vacuum::SingleProduct, IndexSpaceMetric::Unit,
+                              BraKetSymmetry::conjugate, SPBasis::spinorbital));
+
+  setMRCCConvention();
+
+  constexpr std::size_t nAct = 2;
+
+  auto expr = c0dagger(nAct) * (f() + g()) * t() * c0(nAct);
+
+  auto contracted = FWickTheorem{expr}
+                        .set_external_indices(std::array<Index, 0>{})
+                        .full_contractions(true)
+                        .compute();
+
+  std::wcout << to_latex(expr) << "\n=\n" << to_latex(contracted) << std::endl;
+}
